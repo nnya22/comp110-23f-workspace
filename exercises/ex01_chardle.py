8c94fa564794c7819b5e652a429446aa3a2efd4a
Nya Patton <nnya@live.unc.edu>
2023-09-13 12:41:20 -0400

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,7 +2,23 @@
 __author__ = "730324119"
 
 five_c: str = input("Enter a 5-character word: ")
+
+if len(five_c) < 5:
+     print("Error: Word must contain 5 characters")
+     exit()
+if len(five_c) > 5:
+     print("Error: Word must contain 5 characters")
+     exit()
+
 single_c: str = input("Enter a single character: ")
+
+if len(single_c) < 1:
+     print("Error: Character must be a single character.")
+     exit()
+if len(single_c) > 1:
+     print("Error: Character must be a single character.")
+     exit()
+
 
 print("Searching for " + single_c + " in " + five_c)
 
@@ -25,8 +41,8 @@ if five_c[4] == single_c:
 
 
 if instances == 0:
-    print("No instances of" + single_c + "found in" + five_c)
+    print("No instances of " + single_c + " found in " + five_c)
 elif instances == 1:
-        print("1 instance of" + single_c + "found in" + five_c)
+        print("1 instance of " + single_c + " found in " + five_c)
 elif instances >1:
-        print(str(instances) + " instances of " + single_c + " found in a" + five_c)
+        print(str(instances) + " instances of " + single_c + " found in " + five_c)
