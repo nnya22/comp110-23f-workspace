71c8c40502f1b98b3281da5cdfc8b5aa6d4e7fe5
Nya Patton <nnya@live.unc.edu>
2023-10-11 22:09:02 -0400

    Progress on EX04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,12 +1,10 @@
-"""Exercise 4: List Utility Functions Practice"""
+"""Exercise 4: List Utility Functions Practice!"""
 __author__ = "730324119"
 
-test_int: int = 2
-test_list_of_ints: int = [2,2,2]
-test2_list_of_ints: int = [1,2,2]
 
-def all(list_ints, single_int) -> bool:
-    if(len(list_ints)==0):
+def all(list_ints: list[int], single_int: int) -> bool:
+    """Checks the list of integers to ensure every integer matches the single integer."""
+    if (len(list_ints) == 0):
         return False
     index: int = 0
     while index < len(list_ints):
@@ -15,10 +13,12 @@ def all(list_ints, single_int) -> bool:
         index += 1
     return True
 
-def max(list_ints) -> int:
+
+def max(list_ints: list[int]) -> int:
+    """Determines the integer within the list that has the maximum value."""
     if len(list_ints) == 0:
-        raise ValueError('max() arg is an empty List')
-    max_value: int = 0
+        raise ValueError("max() arg is an empty List")
+    max_value: int = list_ints[0]
     index: int = 0
     while index < len(list_ints):
         if list_ints[index] > max_value:
@@ -26,7 +26,9 @@ def max(list_ints) -> int:
         index += 1
     return max_value
 
-def is_equal(list1, list2) -> bool:
+
+def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """Ensures that each each element at every index is equal in both lists."""
     if len(list1) != len(list2):
         return False
     index = 0
@@ -35,10 +37,3 @@ def is_equal(list1, list2) -> bool:
             return False
         index += 1
     return True
-
-def main():
-    print(all(test_list_of_ints, test_int))
-    print(max(test_list_of_ints))
-    print(is_equal(test_list_of_ints, test2_list_of_ints))
-
-main()
